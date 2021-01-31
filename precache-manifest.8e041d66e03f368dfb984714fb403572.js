8f1bbfbeebd55f22d45cd8a20e9b937d0a386dd6
Neha Choudhary <nehachoudhary0814@gmail.com>
2021-02-01 02:06:38 +1300

    Updates

---
+++ precache-manifest.8e041d66e03f368dfb984714fb403572.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316b1c40723fd24f091a1668e293cdda",
+    "url": "/userinterface/index.html"
+  },
+  {
+    "revision": "9083023d2e7c55261074",
+    "url": "/userinterface/static/css/main.44834a7e.chunk.css"
+  },
+  {
+    "revision": "f62f80e82818eacb8f49",
+    "url": "/userinterface/static/js/2.54c9a211.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/userinterface/static/js/2.54c9a211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9083023d2e7c55261074",
+    "url": "/userinterface/static/js/main.cc2f986e.chunk.js"
+  },
+  {
+    "revision": "fd82649a94d8a0895cee",
+    "url": "/userinterface/static/js/runtime-main.48091b04.js"
+  },
+  {
+    "revision": "9248bd2eeca56f1dd8187aa0abd6abb9",
+    "url": "/userinterface/static/media/about.9248bd2e.jpg"
+  },
+  {
+    "revision": "b481ca34c1e92829e7115a12ac3e7378",
+    "url": "/userinterface/static/media/computer.b481ca34.jpg"
+  },
+  {
+    "revision": "1fa237d6adfe6f82592117421538cc76",
+    "url": "/userinterface/static/media/digitization.1fa237d6.jpg"
+  }
+]);
